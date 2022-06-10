1bcf31abd9245f8b362d071ca330e492192a4e6c
David Santoyo Lopez <david.santoyo@Davids-MacBook-Pro.local>
2022-06-10 13:02:50 -0500

    Add SplashFragment logo animations

--- app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
@@ -1,15 +1,17 @@
 package com.wizeline.academy.animations.ui.splash_screen
 
+import android.animation.Animator
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.CycleInterpolator
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.wizeline.academy.animations.databinding.SplashFragmentBinding
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
 
@@ -27,10 +29,43 @@ class SplashFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        lifecycleScope.launch {
-            delay(2000)
+        animate {
             goToHomeScreen()
         }
+    }
+
+    private fun animate(onEnd: () -> Unit = {}) {
+        val firstDuration = 2500L
+        val scaleInterpolator = CycleInterpolator(2f)
+        val max = 1.025f
+        val min = 0.975f
+        val sx = ObjectAnimator.ofFloat(binding.ivWizelineLogo, View.SCALE_X, max, min)
+            .setDuration(firstDuration).apply { interpolator = scaleInterpolator }
+        val sy = ObjectAnimator.ofFloat(binding.ivWizelineLogo, View.SCALE_Y, max, min)
+            .setDuration(firstDuration).apply { interpolator = scaleInterpolator }
+        val secondDuration = 1000L
+        val ry = ObjectAnimator.ofFloat(binding.ivWizelineLogo, View.ROTATION_Y, 0f, 360f * 3)
+            .setDuration(secondDuration)
+        val shrink = ValueAnimator.ofFloat(1f, 0f).setDuration(secondDuration).apply {
+            addUpdateListener {
+                binding.ivWizelineLogo.scaleX = it.animatedValue as Float
+                binding.ivWizelineLogo.scaleY = it.animatedValue as Float
+            }
+        }
+        AnimatorSet().apply {
+            play(sx)
+                .with(sy)
+                .before(ry)
+                .before(shrink)
+            addListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(p0: Animator?) {}
+                override fun onAnimationCancel(p0: Animator?) {}
+                override fun onAnimationRepeat(p0: Animator?) {}
+                override fun onAnimationEnd(p0: Animator?) {
+                    onEnd()
+                }
+            })
+        }.start()
     }
 
     private fun goToHomeScreen() {
