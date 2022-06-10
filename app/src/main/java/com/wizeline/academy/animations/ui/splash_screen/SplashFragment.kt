5e5494d62764a9271913766daa7954a7b741fcec
David Santoyo Lopez <david.santoyo@Davids-MacBook-Pro.local>
2022-06-10 14:50:32 -0500

    Add transition animations from Splash to Main

--- app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/com/wizeline/academy/animations/ui/splash_screen/SplashFragment.kt
@@ -55,13 +55,15 @@ class SplashFragment : Fragment() {
         AnimatorSet().apply {
             play(sx)
                 .with(sy)
-                .before(ry)
-                .before(shrink)
             addListener(object : Animator.AnimatorListener {
                 override fun onAnimationStart(p0: Animator?) {}
                 override fun onAnimationCancel(p0: Animator?) {}
                 override fun onAnimationRepeat(p0: Animator?) {}
                 override fun onAnimationEnd(p0: Animator?) {
+                    AnimatorSet().apply {
+                        play(ry)
+                            .with(shrink)
+                    }.start()
                     onEnd()
                 }
             })
