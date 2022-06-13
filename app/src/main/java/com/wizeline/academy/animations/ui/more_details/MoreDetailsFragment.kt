35e7fae40a29d61157a14ac5d8cfc70b19894ca7
David Santoyo López <david.santoyo@wizeline.com>
2022-06-13 15:08:32 -0500

    Add SpringAnimation for ImageView in MoreDetails

--- app/src/main/java/com/wizeline/academy/animations/ui/more_details/MoreDetailsFragment.kt
+++ app/src/main/java/com/wizeline/academy/animations/ui/more_details/MoreDetailsFragment.kt
@@ -1,9 +1,14 @@
 package com.wizeline.academy.animations.ui.more_details
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.view.ViewGroup
+import androidx.dynamicanimation.animation.DynamicAnimation
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -37,5 +42,44 @@ class MoreDetailsFragment : Fragment() {
         viewModel.title.observe(viewLifecycleOwner) { binding.tvTitle.text = it }
         viewModel.content.observe(viewLifecycleOwner) { binding.tvFullTextContent.text = it }
         viewModel.fetchData(args.contentIndex)
+        setupZoomAnimation()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun setupZoomAnimation() {
+        var scale = 1f
+        val springX = SpringAnimation(binding.ivImageDetailLarge, DynamicAnimation.SCALE_X).apply {
+            spring = createSpringForce()
+        }
+        val springY = SpringAnimation(binding.ivImageDetailLarge, DynamicAnimation.SCALE_Y).apply {
+            spring = createSpringForce()
+        }
+        val sgd = ScaleGestureDetector(requireContext(), object : ScaleGestureDetector.OnScaleGestureListener {
+            override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
+                springX.cancel()
+                springY.cancel()
+                return true
+            }
+            override fun onScale(detector: ScaleGestureDetector): Boolean {
+                scale *= detector.scaleFactor
+                binding.ivImageDetailLarge.scaleX *= scale
+                binding.ivImageDetailLarge.scaleY *= scale
+                return true
+            }
+            override fun onScaleEnd(detector: ScaleGestureDetector?) {
+                springX.start()
+                springY.start()
+            }
+        })
+        binding.ivImageDetailLarge.setOnTouchListener { _, motionEvent ->
+            sgd.onTouchEvent(motionEvent)
+        }
+    }
+
+    private fun createSpringForce(): SpringForce {
+        return SpringForce(1f).apply {
+            dampingRatio = SpringForce.DAMPING_RATIO_MEDIUM_BOUNCY
+            stiffness = SpringForce.STIFFNESS_LOW
+        }
     }
 }
